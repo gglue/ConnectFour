07c6b9315486af17d384b405cb2933c9aaa423bf
gglue <generalglue@gmail.com>
2023-02-22 07:36:20 -0500

    Finished UDP Implementation & Signal Handler

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import signal
+
 import numpy as np
 from socket import *
 ROW_NUMBER = 6
 COLUMN_NUMBER = 7
+IP_ADDRESS = 'localhost'
 
 def printBoard(board):
     """
@@ -27,7 +30,9 @@ def dropToken(board, columnNumber, player):
     :param player: Which player dropped the token
     :return: The row where the token is dropped
     """
-    currentRow = 5
+    if columnNumber > COLUMN_NUMBER - 1:
+        return None
+    currentRow = ROW_NUMBER - 1
     for row in reversed(board):
         if row[columnNumber] == 0:
             row[columnNumber] = player
@@ -106,39 +111,67 @@ def checkWinCondition(board, r, c, player):
     if diagonalBRWin == 4: return 'bottomRight'
     return None
 
+def checkBoard(board, player):
+    """
+    This function checks the entire ConnectFour board for a win condition
+    :param player: The player who placed the token
+    :param board: The ConnectFour board
+    :return: A boolean variable representing whether a win condition was found or not
+    """
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            winCon = checkWinCondition(board, x, y, player)
+            if winCon:
+                return winCon
+
 def localVersus():
     """
     This function is used when user wants to play local multiplayer
     :return: null
     """
+
+    # Start fresh board
     connectFourBoard = np.zeros((ROW_NUMBER, COLUMN_NUMBER))
-    playerTurn = True
     winCon = None
-    printBoard(connectFourBoard)
+    playerTurn = True
 
     while not winCon:
+        # Change token value depending on player turn
         if playerTurn:
             token = 1
         else:
             token = 2
 
+        printBoard(connectFourBoard)
         print(f"Player {token}'s turn")
-        playerInput = int(input("Enter column number: "))
-        dropToken(connectFourBoard, playerInput, token)
 
-        for x in range(connectFourBoard.shape[0]):
-            for y in range(connectFourBoard.shape[1]):
-                winCon = checkWinCondition(connectFourBoard, x, y, token)
-                if winCon:
-                    break
+        # Input validation to place token
+        playerInput = input("Enter column number: ")
+        if playerInput.isdigit() and int(playerInput) < COLUMN_NUMBER:
+            dropToken(connectFourBoard, int(playerInput), token)
+
+            # Check if opponent has won yet
+            winCon = checkBoard(connectFourBoard, token)
             if winCon:
                 print(f"Player {token} wins because of {winCon}")
-                break
 
-        playerTurn = not playerTurn
-        printBoard(connectFourBoard)
+            # Switch to other player's turn
+            playerTurn = not playerTurn
+
 
 def onlineVersus():
+
+    def handler(signum, frame):
+        """This function is the signal handler for a SIGINT signal"""
+
+        print("Interrupt received, shutting down...")
+
+        # Drop all inventory before exit
+        userSocket.close()
+
+        # Exit program
+        quit()
+
     """
     This function is used when user wants to play multiplayer online
     :return:
@@ -146,24 +179,106 @@ def onlineVersus():
     # Set user address info
     userSocket = socket(AF_INET, SOCK_DGRAM)
     userPort = int(input("Please enter your desired port number: "))
-    userSocket.bind(('localhost', userPort))
+    userSocket.bind((IP_ADDRESS, userPort))
+
+    # Create a signal for interruption
+    signal.signal(signal.SIGINT, handler)
 
     print("Are you hosting or are you connecting?")
     print("1. Connecting")
     print("2. Hosting")
     playerNumber = int(input("Select your answer (#): "))
 
+    # Player 1 goes second
     if playerNumber == 1:
         opponentPort = int(input("Please enter opponent's port number: "))
-        userSocket.sendto("Hello".encode(), ('localhost', opponentPort))
+        userSocket.sendto("Connected".encode(), (IP_ADDRESS, opponentPort))
 
-    else:
-        print(f"You are hosting on {userSocket.getsockname()}")
-        while True:
+        # Start fresh board
+        connectFourBoard = np.zeros((ROW_NUMBER, COLUMN_NUMBER))
+        winCon = None
+        printBoard(connectFourBoard)
+
+        while not winCon:
+            # Get opponent's input
+            print(f"Opponent's turn, please wait")
             message, clientAddress = userSocket.recvfrom(2048)
             userMessage = message.decode()
-            print(userMessage)
+            dropToken(connectFourBoard, int(userMessage), 2)
 
+            # Check if opponent has won yet
+            printBoard(connectFourBoard)
+            winCon = checkBoard(connectFourBoard, 2)
+            if winCon:
+                print(f"Player 2 wins because of {winCon}")
+                break
+
+            # If opponent has not won, let player play his turn
+            else:
+                # Input validation to place token
+                while True:
+                    playerInput = (input("It's your turn; Enter column number: "))
+                    if playerInput.isdigit() and int(playerInput) < COLUMN_NUMBER:
+                        break
+
+                # Drop token and send input to opponent
+                dropToken(connectFourBoard, int(playerInput), 1)
+                printBoard(connectFourBoard)
+                userSocket.sendto(playerInput.encode(), (IP_ADDRESS, opponentPort))
+
+                # Check for win condition
+                winCon = checkBoard(connectFourBoard, 1)
+                if winCon:
+                    print(f"Player 1 wins because of {winCon}")
+                    break
+
+    # Player 2 goes first
+    else:
+        # Gets opponent's ip address and port for future communication
+        print(f"You are hosting on {userSocket.getsockname()}")
+        message, clientAddress = userSocket.recvfrom(2048)
+        opponentPort = clientAddress[1]
+        userMessage = message.decode()
+
+        if "Connected" in userMessage:
+
+            # Start fresh board
+            connectFourBoard = np.zeros((ROW_NUMBER, COLUMN_NUMBER))
+            winCon = None
+            printBoard(connectFourBoard)
+
+            while not winCon:
+                # Input validation to place token
+                while True:
+                    playerInput = (input("It's your turn; Enter column number: "))
+                    if playerInput.isdigit() and int(playerInput) < COLUMN_NUMBER:
+                        break
+
+                # Drop token and send input to opponent
+                dropToken(connectFourBoard, int(playerInput), 2)
+                printBoard(connectFourBoard)
+                userSocket.sendto(playerInput.encode(), (IP_ADDRESS, opponentPort))
+
+                # Check for win condition
+                winCon = checkBoard(connectFourBoard, 2)
+                if winCon:
+                    print(f"Player 2 wins because of {winCon}")
+                    break
+
+                # If no win condition, it's the opponent's turn
+                else:
+                    # Get opponent's input
+                    print(f"Opponent's turn, please wait")
+                    message, clientAddress = userSocket.recvfrom(2048)
+                    userMessage = message.decode()
+                    dropToken(connectFourBoard, int(userMessage), 1)
+
+                    # Check if opponent has won yet
+                    printBoard(connectFourBoard)
+                    winCon = checkBoard(connectFourBoard, 1)
+                    if winCon:
+                        print(f"Player 1 wins because of {winCon}")
+                        break
 
 if __name__ == '__main__':
     """
